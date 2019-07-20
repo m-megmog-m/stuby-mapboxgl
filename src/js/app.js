3f035995aef2d2c772a69f75aaab6b3f4aa1b35e
megmog <m.megmog.m@gmail.com>
2019-07-20 14:28:31 +0900

    Initial commit

---
+++ src/js/app.js
@@ -0,0 +1,27 @@
+const map = new mapboxgl.Map({
+  container: "map",
+  style: {
+    version: 8,
+    sources: {
+      m_mono: {
+        type: "raster",
+        tiles: ["https://tile.mierune.co.jp/mierune_mono/{z}/{x}/{y}.png"],
+        tileSize: 256
+      }
+    },
+    layers: [{
+      id: "m_mono",
+      type: "raster",
+      source: "m_mono",
+      minzoom: 0,
+      maxzoom: 18
+    }]
+  },
+  center: [139.7670, 35.6810],
+  zoom: 13
+});
+
+map.on("load", function() {});
+
+const nc = new mapboxgl.NavigationControl();
+map.addControl(nc, 'top-left');
